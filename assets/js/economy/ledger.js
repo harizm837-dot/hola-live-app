60b2cc4ef5fbe048dfc27ec840ce118347df4b73
harizm837-dot <harizm837@gmail.com>
2026-02-13 19:06:24 +0800

    Update ledger.js

--- assets/js/economy/ledger.js
+++ assets/js/economy/ledger.js
@@ -1,15 +1,21 @@
 const ledgerKey = "hola_ledger";
 
 export const Ledger = {
+
   getAll(){
     return JSON.parse(localStorage.getItem(ledgerKey)) || [];
   },
+
   addTransaction(tx){
     const all = Ledger.getAll();
     all.push(tx);
     localStorage.setItem(ledgerKey, JSON.stringify(all));
   },
+
   getTransactionsFor(userId){
-    return Ledger.getAll().filter(tx => tx.from===userId || tx.to===userId);
+    return Ledger.getAll().filter(
+      tx => tx.from === userId || tx.to === userId
+    );
   }
+
 };
