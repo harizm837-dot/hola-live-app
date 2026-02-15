36382cdbc871dfc92b0bb8957422cba09d2ab1e7
harizm837-dot <harizm837@gmail.com>
2026-02-15 12:30:07 +0800

    Add ModeratorEngine with mute and ban functions

---
+++ assets/js/features/moderator.js
@@ -0,0 +1,11 @@
+window.ModeratorEngine = {
+
+  muteUser(username){
+    Notify.toast(username + " mute хийгдлээ");
+  },
+
+  banUser(username){
+    Notify.toast(username + " ban хийгдлээ");
+  }
+
+};
